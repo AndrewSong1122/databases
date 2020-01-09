d449e6cf66557c5825e47db3d3bebdd153d070ab
AndrewSong1122 <andrew.song.1122@gmail.com>
2020-01-08 17:50:11 -0800

    Work on server attempted
    Continuing sprint on the morrow

--- server/schema.sql
+++ server/schema.sql
@@ -3,10 +3,41 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  /* This table holds the messages for storage on the server.
+     Some keys are shared with other tables .*/
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  message_text TEXT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(user_id)
+    REFERENCES users(id),
+  FOREIGN KEY(room_id)
+    REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_name TEXT NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  room_name TEXT NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE friends (
+  user1 INT NOT NULL,
+  user2 INT NOT NULL,
+  FOREIGN KEY(user1)
+    REFERENCES users(id),
+  FOREIGN KEY(user2)
+    REFERENCES users(id)
+);
 
 
 
